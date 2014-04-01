93c94cdbff99ee6e3eba6ec70cd19bf2d13d55d1
TurpIF <pierre.turpin@insa-lyon.fr>
2014-04-01 13:59:57 +0200

    Implementation basique de Element

---
+++ Element.cpp
@@ -0,0 +1,17 @@
+#include "Element.h"
+#include "Attribute.h"
+
+Element::Element(std::vector<Attribute *> const & a):
+  _attributes(a)
+{
+}
+
+Element::~Element() {
+  for (std::vector<Attribute *>::iterator it = _attributes.begin();
+      it != _attributes.end(); it++)
+    delete *it;
+}
+
+std::vector<Attribute *> const & Element::attributes() const {
+  return _attributes;
+}
--- Element.h
+++ Element.h
@@ -11,6 +11,11 @@ class Element {
   public:
     Element(std::vector<Attribute *> const &);
     virtual ~Element();
+
+    std::vector<Attribute *> const & attributes() const;
+
+  protected:
+    std::vector<Attribute *> _attributes;
 };
 
 #endif
