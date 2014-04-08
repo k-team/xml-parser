14302c215490f291c0abe2599b4f4d8c7b122da6
Carensac Samuel <scarensac@iftpserv2.insa-lyon.fr>
2014-04-08 10:03:34 +0200

    base class

---
+++ base.cpp
@@ -0,0 +1,7 @@
+#include "base.h"
+
+Document::~Document() {
+}
+
+Document::Document() {
+}
---
+++ base.h
@@ -0,0 +1,14 @@
+#ifndef BASE_H
+#define BASE_H
+
+
+class Base {
+  public:
+    Base();
+    virtual ~Base();
+
+    virtual bool to_be_or_not_to_be()=0;
+    
+};
+
+#endif
