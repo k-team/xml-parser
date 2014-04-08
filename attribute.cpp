7c74baba1202ce499e02c7a6f60daa391070edfc
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-04-08 11:16:47 +0200

    CHMOD

--- attribute.cpp
+++ attribute.cpp
@@ -1,18 +1,22 @@
 #include "attribute.h"
 
-Attribute::Attribute(Attribute::name_type const & n, Attribute::value_type const & v):
-  _name(n), _value(v)
+Attribute::Attribute(Attribute::name_type const & name,
+    Attribute::value_type const & value):
+  _name(name), _value(value)
 {
 }
 
-Attribute::~Attribute() {
+Attribute::~Attribute()
+{
 }
 
-Attribute::name_type const & Attribute::name() const {
+Attribute::name_type const & Attribute::name() const
+{
   return _name;
 }
 
-Attribute::value_type const & Attribute::value() const {
+Attribute::value_type const & Attribute::value() const
+{
   return _value;
 }
 
