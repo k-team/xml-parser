f1d80321b1b594e0d8878ba5a6feee9ff89272f8
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-04-08 15:37:32 +0200

    TODO

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <algorithm>
 #include <map>
 #include <iostream>
+#include <fstream>
+#include <cstdio>
+#include "document.h"
+#include "xml.tab.h"
 
-extern int xmlparse(void);
+extern FILE * xmlin;
+extern int xmlparse(Document **);
 
 static std::string help_message = \
 "                                                                             \n\
@@ -37,7 +42,8 @@ int main(int argc, const char ** argv)
   if (argc < 2)
   {
     std::cerr << "Missing option parameter!" << std::endl
-      << help_message << std::endl;
+      //<< help_message << std::endl
+    ;
     return EXIT_FAILURE;
   }
 
@@ -51,32 +57,98 @@ int main(int argc, const char ** argv)
     return EXIT_FAILURE;
   }
 
-  // Call command handler
+  // Call command handler, making argc and argv correspond
+  int nb_ignored = 2;
+  argc -= nb_ignored;
+  argv += nb_ignored;
+  std::cout << argc << std::endl;
   return it->second(argc, argv);
 }
 
-int handle_help(int argc, const char ** argv)
+int handle_help(int, const char **)
 {
   std::cout << help_message << std::endl;
   return 0;
 }
 
+static Document * read_document(const char * fname)
+{
+  // Change xmlparse input
+  FILE * fp = fopen(fname, "r");
+  if (fp == nullptr)
+  {
+    return nullptr;
+  }
+  xmlin = fp;
+
+  // Construct document
+  Document * doc = nullptr;
+  xmlparse(&doc); // who gives a f**k about the return code ?
+  return doc;
+}
+
 int handle_validate(int argc, const char ** argv)
 {
+  if (argc < 1)
+  {
+    std::cerr << "Filename argument required" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Open and handle errors
+  Document * doc = read_document(argv[0]);
   // TODO
-  return 1;
+  delete doc;
+
+  return EXIT_FAILURE;
 }
 
 int handle_process(int argc, const char ** argv)
 {
-  // TODO
-  return 1;
+  if (argc < 1)
+  {
+    std::cerr << "Filename argument required" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Open and handle errors
+  Document * doc = read_document(argv[0]);
+  if (doc == nullptr)
+  {
+    // TODO error message ?
+    return EXIT_FAILURE;
+  }
+
+  std::cout << doc->str() << std::endl;
+  delete doc;
+
+  return EXIT_FAILURE;
 }
 
 int handle_transform(int argc, const char ** argv)
 {
+  if (argc < 1)
+  {
+    std::cerr << "XML filename argument required" << std::endl;
+    return EXIT_FAILURE;
+  }
+  else if (argc < 2)
+  {
+    std::cerr << "XSL filename argument required" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Open and handle errors
+  Document * xml_doc = read_document(argv[0]);
   // TODO
-  return 1;
+  delete xml_doc;
+
+  // Open and handle errors
+  Document * xsl_doc = read_document(argv[1]);
+  // TODO
+  delete xsl_doc;
+
+  return EXIT_FAILURE;
 }
 
 // vim:ft=cpp et sw=2 sts=2:
